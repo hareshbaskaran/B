9f6d518831ba4052388d0b0190cd0ba9db369e49
haresh <haresh@codez.ai>
2024-04-11 20:36:08 +0530

    version --fix

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
-from tensorflow.keras.applications.resnet50 import ResNet50
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
 import numpy as np
 from PIL import Image
 from io import BytesIO
