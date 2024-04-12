402c43aa4c02eb67d8cd3fa3bb3506a04848c536
haresh <haresh@codez.ai>
2024-04-12 11:36:09 +0530

    add - tesing api

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+from PIL import Image
 from predict import read_image
 from predict import transformacao
-from fastapi import FastAPI, File
+from fastapi import FastAPI, File, UploadFile
+from io import BytesIO
 
 app = FastAPI()
 
 
+@app.get("/")
+async def root():
+    return {"message": "Api for BlindBuddy"}
+
+@app.get("/test")
+async def root():
+    return {"message": "Hello World"}
 @app.post("/files/")
 async def create_file(file: bytes = File(...)):
     return {"file_size": len(file)}
